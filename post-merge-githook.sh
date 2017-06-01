2272886580f5d825e3b57bee5ffef0abf3297647
Basile Starynkevitch <basile@starynkevitch.net>
2017-06-01 14:24:43 +0200

    post-merge-githook.sh & pre-commit-githook.sh use dump & restore

--- post-merge-githook.sh
+++ post-merge-githook.sh
@@ -2,5 +2,5 @@
 # file post-merge-githook.sh to be installed as a git hook in .git/hooks/post-merge
 # also invoked thru git pull
 echo start post-merge-githook.sh "$@"
-make restorestate
+make restore
 echo end post-merge-githook.sh "$@"
--- pre-commit-githook.sh
+++ pre-commit-githook.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # file pre-commit-githook.sh to be installed as a git hook in .git/hooks/pre-commit
 echo start pre-commit-githook.sh "$@"
-make dumpstate
+make dump
 echo end pre-commit-githook.sh "$@"
