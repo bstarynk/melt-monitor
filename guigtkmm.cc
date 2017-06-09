ac4d31f61304c206fe4abab0028b38c5b704973f
Basile Starynkevitch <basile@starynkevitch.net>
2017-06-09 03:51:47 +0200

    begin adding MomMainWindow, very ugly

--- guigtkmm.cc
+++ guigtkmm.cc
@@ -22,11 +22,51 @@
 
 #include <gtkmm.h>
 
+class MomMainWindow : public Gtk::ApplicationWindow
+{
+  Gtk::Box _vbox;
+  Glib::RefPtr<Gtk::TextBuffer> _buf;
+  Gtk::Paned _panedtx;
+  Gtk::ScrolledWindow _scrwtop;
+  Gtk::ScrolledWindow _scrwbot;
+  Gtk::TextView _txvtop;
+  Gtk::TextView _txvbot;
+  Gtk::TextView _txvcmd;
+public:
+  MomMainWindow(const Glib::RefPtr<Gtk::Application>&app);
+  ~MomMainWindow();
+};				// end class MomMainWindow
+
+MomMainWindow::MomMainWindow(const Glib::RefPtr<Gtk::Application>&app)
+  : Gtk::ApplicationWindow(app),
+    _vbox(Gtk::ORIENTATION_VERTICAL),
+    _buf(Gtk::TextBuffer::create()),
+    _panedtx(Gtk::ORIENTATION_VERTICAL),
+    _txvtop(_buf), _txvbot(_buf),
+    _txvcmd()
+{
+  add(_vbox);
+  _vbox.pack_start(_panedtx,Gtk::PACK_SHRINK);
+  _scrwtop.add(_txvtop);
+  _scrwtop.set_policy(Gtk::POLICY_AUTOMATIC,Gtk::POLICY_ALWAYS);
+  _scrwbot.add(_txvbot);
+  _scrwbot.set_policy(Gtk::POLICY_AUTOMATIC,Gtk::POLICY_ALWAYS);
+  _panedtx.add1(_scrwtop);
+  _panedtx.add2(_scrwbot);
+  _vbox.pack_start(_txvcmd,Gtk::PACK_SHRINK);
+  show_all_children();
+};				// end MomMainWindow::MomMainWindow
+
+
+MomMainWindow::~MomMainWindow()
+{
+};				// end MomMainWindow::~MomMainWindow
+
 int
 mom_run_gtkmm_gui(int& argc, char**argv)
 {
   auto app = Gtk::Application::create(argc, argv, "org.gcc-melt.monitor");
-  Gtk::Window window;
+  MomMainWindow window(app);
   MOM_INFORMPRINTF("running mom_run_gtkmm_gui");
   window.set_default_size(200,300);
   return app->run(window);
